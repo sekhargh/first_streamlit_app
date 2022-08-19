3887c216995b7f46c278d78640f8c2dbbb4a0539
sekhargh <111313528+sekhargh@users.noreply.github.com>
2022-08-19 11:55:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
-print("hellow")
+import streamlit
+streamlit.title ("My new healthy Dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry oatmeal")
+streamlit.text("kale,Spinach and Rocket smoothie")
+streamlit.text("Hard-Boiled Free range egg")
