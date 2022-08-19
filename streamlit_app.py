6fe27d5c9d1877839f422235eb4045fa9255141b
sekhargh <111313528+sekhargh@users.noreply.github.com>
2022-08-19 12:04:32 -0500

    Update streamlit_app.py
    added one more header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
 streamlit.title ("My new healthy Dinner")
+
 streamlit.header("Breakfast Favorites")
+
 streamlit.text("🥣 Omega 3 and Blueberry oatmeal")
 streamlit.text("🥗 kale,Spinach and Rocket smoothie")
 streamlit.text("🐔 Hard-Boiled Free range egg")
 streamlit.text("🥑🍞 Avacado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
